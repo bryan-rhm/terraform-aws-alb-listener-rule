741d57cf3c97a0ac4f15b28d6c8c1f7e29889fcd
Bryan Recinos <bryan.rhm1@gmail.com>
2022-06-06 16:33:43 -0600

    [v1.1.0] removed order

--- main.tf
+++ main.tf
@@ -2,9 +2,23 @@ resource "aws_lb_listener_rule" "this" {
   listener_arn = var.listener_arn
   priority     = var.priority
 
+  dynamic "action" {
+    for_each = var.oidc_config != null ? [1] : []
+    content {
+      type = "authenticate-oidc"
+      authenticate_oidc {
+        authorization_endpoint = var.oidc_config.authorization_endpoint
+        client_id              = var.oidc_config.client_id
+        client_secret          = var.oidc_config.client_secret
+        issuer                 = var.oidc_config.issuer
+        token_endpoint         = var.oidc_config.token_endpoint
+        user_info_endpoint     = var.oidc_config.user_info_endpoint
+      }
+    }
+  }
+
   action {
     type             = "forward"
-    order            = 10
     target_group_arn = length(var.target_groups) == 1 ? var.target_groups[0].arn : null
 
     dynamic "forward" {
@@ -17,22 +31,6 @@ resource "aws_lb_listener_rule" "this" {
             weight = target_group.value.weight
           }
         }
-      }
-    }
-  }
-
-  dynamic "action" {
-    for_each = var.oidc_config != null ? [1] : []
-    content {
-      type = "authenticate-oidc"
-      order = 4
-      authenticate_oidc {
-        authorization_endpoint = var.oidc_config.authorization_endpoint
-        client_id              = var.oidc_config.client_id
-        client_secret          = var.oidc_config.client_secret
-        issuer                 = var.oidc_config.issuer
-        token_endpoint         = var.oidc_config.token_endpoint
-        user_info_endpoint     = var.oidc_config.user_info_endpoint
       }
     }
   }
